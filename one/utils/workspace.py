75f169098fe20be949f0d23f60b72f866e8d5670
Allan Denot <adenot@gmail.com>
2020-05-06 16:03:26 +1000

    Simplifying workspace definition, using keys as workspace name

--- one/utils/workspace.py
+++ one/utils/workspace.py
@@ -1,15 +1,13 @@
 import yaml
 from os import path
 
-
 def get_workspaces():
     workspaces = []
     if path.exists('./one.yaml'):
         with open('./one.yaml') as file:
             docs = yaml.load(file, Loader=yaml.FullLoader)
-            for workspace in docs['workspaces']:
-                for key in workspace.keys():
-                    workspaces.append(key)
+            for workspace_key in docs['workspaces'].keys():
+                workspaces.append(workspace_key)
         file.close()
     else:
         print('No config file in current directory.')
@@ -22,10 +20,9 @@ def get_workspace_value(workspace, variable):
     if path.exists('./one.yaml'):
         with open('./one.yaml') as file:
             docs = yaml.load(file, Loader=yaml.FullLoader)
-            for doc in docs['workspaces']:
-                for key, value in doc.items():
-                    if key == workspace:
-                        return value[variable]
+            for workspace_key in docs['workspaces'].keys():
+                if workspace_key == workspace:
+                    return docs['workspaces'][workspace_key]
         file.close()
     else:
         print('No config file in current directory.')
