b90675ce632a561949b792d2459ead94fdf1ac75
Arthur Diniz <arthurbdiniz@gmail.com>
2020-06-18 19:23:23 -0300

    Update download script with cases for each OS
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- get_one.sh
+++ get_one.sh
@@ -1,13 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]
-then
-    sudo curl -L https://github.com/DNXLabs/one-cli/releases/latest/download/one_linux_amd64 -o /usr/local/bin/one
-elif [[ "$OSTYPE" == "darwin"* ]]
-then
+os=${OSTYPE//[0-9.-]*/}
+
+case "$os" in
+  darwin)
     sudo curl -L https://github.com/DNXLabs/one-cli/releases/latest/download/one_macos_amd64 -o /usr/local/bin/one
-else
-    echo 'No support for this platform'
-fi
+    ;;
+
+  msys)
+    echo 'No support for Windows platform'
+    ;;
+
+  linux)
+    sudo curl -L https://github.com/DNXLabs/one-cli/releases/latest/download/one_linux_amd64 -o /usr/local/bin/one
+    ;;
+  *)
+
+  echo "Unknown Operating system $OSTYPE"
+  exit 1
+esac
 
 sudo chmod +x /usr/local/bin/one
