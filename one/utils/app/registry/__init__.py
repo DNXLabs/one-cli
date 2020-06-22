cb14568ca49ff333bfaa1544a65288506f46fd13
Arthur Diniz <arthurbdiniz@gmail.com>
2020-06-21 23:40:21 -0300

    Makes prompt, app deploy and app registry modules
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

---
+++ one/utils/app/registry/__init__.py
@@ -0,0 +1,28 @@
+import click
+import subprocess
+from one.utils.config import get_config_value
+
+
+class App:
+    pass
+
+
+class AppRegistry:
+    def __init__(self):
+        self.name = get_config_value('app.name')
+        self.image_name = get_config_value('app.docker.image-name', get_config_value('app.name'))
+        self.dockerfile = get_config_value('app.docker.file', 'Dockerfile')
+        self.build_cmd_args = get_config_value('app.docker.build-cmd-args', '')
+
+    def get_image_tag(self, build_version):
+        return "%s:%s" % (self.image_name, build_version)
+
+    def docker_build_raw(self, image):
+        command = ['docker', 'build', '-t', image, '-f', self.dockerfile] + self.build_cmd_args.split(' ') + ['.']
+        click.echo(" ".join(command))
+        subprocess.call(list(filter(None, command)))
+
+    def docker_push_raw(self, image):
+        command = ['docker', 'push', image]
+        click.echo(" ".join(command))
+        subprocess.call(list(filter(None, command)))
