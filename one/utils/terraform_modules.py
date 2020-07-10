1327cf3d2fcb753f5793185ee87ccf386dc1ea6f
Arthur Diniz <arthurbdiniz@gmail.com>
2020-07-10 06:45:50 -0300

    Add test to terraform modules check feature
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- one/utils/terraform_modules.py
+++ one/utils/terraform_modules.py
@@ -5,48 +5,65 @@ import requests
 from os import path
 
 
-def terraform_modules_check():
-    file_path = '.terraform/modules/modules.json'
+def get_modules_list(url='https://modules.dnx.one/api.json'):
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise SystemExit
+    return response.json()
+
+
+def check_file_path(file_path):
     if not path.exists(file_path):
         raise SystemExit
 
-    response = requests.get('https://modules.dnx.one/api.json')
-    if response.status_code != 200:
-        raise SystemExit
-    api = response.json()
 
-    with open('.terraform/modules/modules.json') as modules_json_file:
+def terraform_modules_check(file_path='.terraform/modules/modules.json', api=None):
+    check_file_path(file_path)
+
+    json_api = api or get_modules_list()
+
+    results = {}
+
+    with open(file_path) as modules_json_file:
         data = json.load(modules_json_file)
         click.echo(
             click.style('\nInitializing DNX modules check...', bold=True)
         )
-        for module in data['Modules']:
+        try:
+            for module in data['Modules']:
 
-            if not module['Source']:
-                continue
-
-            split = re.split(r'[./]\s*', module['Source'])
-            if len(split) >= 5 and split[4] == 'DNXLabs':
-                name = re.split(r'[./]\s*', module['Source'])[5]
-                version = module['Source'].split('=')[1]
-                key = module['Key']
-                api_version = ''
-                try:
-                    api_version = api['modules'][name]['tag_name']
-                except KeyError:
-                    click.echo(
-                        click.style('ERROR ', fg='red') +
-                        'Could not find module ' + name + ' at DNX modules API.'
-                    )
-                if api_version != version:
-                    click.echo(
-                        '- ' + name + '/' + key + ': ' +
-                        click.style(version, fg='yellow') +
-                        ' ~> ' +
-                        click.style(api_version, fg='green')
-                    )
-                else:
-                    click.echo(
-                        '- ' + name + '/' + key + ': ' +
-                        click.style(version, fg='green')
-                    )
+                if not module['Source']:
+                    continue
+                split = re.split(r'[./]\s*', module['Source'])
+                if len(split) >= 5 and split[4] == 'DNXLabs':
+                    name = re.split(r'[./]\s*', module['Source'])[5]
+                    version = module['Source'].split('=')[1]
+                    key = module['Key']
+                    api_version = ''
+                    try:
+                        api_version = json_api['modules'][name]['tag_name']
+                    except KeyError:
+                        click.echo(
+                            click.style('ERROR ', fg='red') +
+                            'Could not find module ' + name + ' at DNX modules API.'
+                        )
+                    if api_version != version:
+                        results[name] = {"key": key, "version": version, "api_version": api_version}
+                        click.echo(
+                            '- ' + name + '/' + key + ': ' +
+                            click.style(version, fg='yellow') +
+                            ' ~> ' +
+                            click.style(api_version, fg='green')
+                        )
+                    else:
+                        results[name] = {"key": key, "version": version}
+                        click.echo(
+                            '- ' + name + '/' + key + ': ' +
+                            click.style(version, fg='green')
+                        )
+        except KeyError:
+            click.echo(
+                click.style('ERROR ', fg='red') +
+                'Couldn not find data from local terraform modules'
+            )
+    return results
