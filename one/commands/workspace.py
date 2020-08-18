b501005d926d49f8ee31d3d15a44bb213ed0f258
Renato Tavares <rtavares@cochlear.com>
2020-08-19 09:40:18 +1000

    👾 print erro msg when do not find workspace  in option

--- one/commands/workspace.py
+++ one/commands/workspace.py
@@ -19,29 +19,37 @@ def list_workspaces():
 @workspace.command(help='Change environment variables to another workspace.')
 @click.option('-n', '--name', default=None, type=str, help='Workspace name.')
 def change(name):
-
     workspaces = get_workspaces()
 
-    if name in workspaces:
+    if name == None:
+        selected_workspace = workspace_question(workspaces)
+    elif name in workspaces:
         selected_workspace = name
         click.echo('Selected workspace: ' + click.style(name, fg='red'))
     else:
-        workspaces_obj = []
-        for workspace in workspaces:
-            workspaces_obj.append({'name': workspace})
-
-        questions = [
-            {
-                'type': 'list',
-                'message': 'Select workspace',
-                'name': 'workspace',
-                'choices': workspaces_obj
-            }
-        ]
-
-        answers = prompt(questions, style=style)
-        selected_workspace = answers['workspace']
+        click.echo('Workspace not found.', err=True)
+        selected_workspace = workspace_question(workspaces)
 
     f = open('.one.workspace', 'w')
     f.write('WORKSPACE=' + selected_workspace + '\n')
     f.close()
+
+def workspace_question(workspaces):
+    workspaces_obj = []
+    for workspace in workspaces:
+        workspaces_obj.append({'name': workspace})
+
+    questions = [
+        {
+            'type': 'list',
+            'message': 'Select workspace',
+            'name': 'workspace',
+            'choices': workspaces_obj
+        }
+    ]
+
+    answers = prompt(questions, style=style)
+    selected_workspace = answers['workspace']
+    
+    return selected_workspace
+
