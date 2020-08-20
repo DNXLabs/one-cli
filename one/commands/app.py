edce3b3c19e3af170eb7d1837381647c56d1f962
Arthur Diniz <arthurbdiniz@gmail.com>
2020-08-20 16:46:12 -0300

    Adds static app deploy feature
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- one/commands/app.py
+++ one/commands/app.py
@@ -18,30 +18,30 @@ def app():
     pass
 
 
-@app.command(name='docker-build', help='Build docker image for deployment')
-@click.option('--build-version', default='latest', help='Build version, used as tag for docker image')
+@app.command(name='docker-build', help='Build docker image for deployment.')
+@click.option('--build-version', default='latest', help='Build version, used as tag for docker image.')
 def docker_build(build_version):
     app_registry = app_registry_factory(get_config_value('app.docker.registry-type', 'ecr'))
     app_registry.docker_build(build_version)
 
 
-@app.command(name='docker-login', help='Login into docker registry')
+@app.command(name='docker-login', help='Login into docker registry.')
 def docker_login():
     app_registry = app_registry_factory(get_config_value('app.docker.registry-type', 'ecr'))
     app_registry.docker_login(environment)
 
 
-@app.command(name='docker-push', help='Push image to docker registry')
-@click.option('--build-version', default='latest', help='Build version, used as tag for docker image')
+@app.command(name='docker-push', help='Push image to docker registry.')
+@click.option('--build-version', default='latest', help='Build version, used as tag for docker image.')
 def docker_push(build_version):
     app_registry = app_registry_factory(get_config_value('app.docker.registry-type', 'ecr'))
     app_registry.docker_push(build_version)
 
 
-@app.command(name='deploy', help='Deploy application')
-@click.option('-w', '--workspace', required=True, help='Workspace to deploy')
-@click.option('--build-version', default='latest', help='Build version to deploy (default: latest)')
-def deploy(workspace, build_version):
+@app.command(name='deploy-ecs', help='Deploy application to ECS.')
+@click.option('-w', '--workspace', required=True, help='Workspace to deploy.')
+@click.option('--build-version', default='latest', help='Build version to deploy (default: latest).')
+def deploy_ecs(workspace, build_version):
     app_deploy = app_deploy_factory(get_workspace_value(workspace, 'type', 'ecs'))
     app_registry = app_registry_factory(get_config_value('app.docker.registry-type', 'ecr'))
 
@@ -51,3 +51,15 @@ def deploy(workspace, build_version):
     click.echo('Deploying %s to %s' % (image_name, workspace))
 
     app_deploy.deploy(environment, workspace, image_name)
+
+
+@app.command(name='deploy-static', help='Deploy static application to S3 bucket.')
+@click.option('-w', '--workspace', default=None, help='Workspace to deploy.')
+def deploy_static(workspace):
+    app_deploy = app_deploy_factory('static')
+
+    environments = environment.build(workspace).get_env()
+
+    click.echo('Deploying static app to %s' % (environments.get('WORKSPACE', 'default')))
+
+    app_deploy.deploy(environments)
