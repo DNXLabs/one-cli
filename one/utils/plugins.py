bf1dae8ddda32f5c46b8e0061aedc95ace42cfc0
Allan Denot <adenot@gmail.com>
2020-08-21 14:01:40 +1000

    Plugins organised by directories, allowing more complex plugins (related to #50)

--- one/utils/plugins.py
+++ one/utils/plugins.py
@@ -7,15 +7,10 @@ from pathlib import Path
 
 home = str(Path.home())
 
-
 def load_plugins(source=home+'/.one/plugins/'):
     if path.exists(source):
-        sys.path.append(home+'/.one')
-        device_directories = glob.glob(join(source, "*.py"))
-        if device_directories and path.exists(source + '__init__.py'):
-            device_directories.remove(source + '__init__.py')
-            for directory in device_directories:
-                command_path = '.'.join(directory.rsplit('/', 2)[1:])[:-3]
-                mod = importlib.import_module(command_path)
-                __init__ = getattr(mod, '__init__')
-                __init__()
+        sys.path.append(source)
+        for directory in glob.glob(join(source, "*/")):
+            if path.exists(directory + '__init__.py'):
+                plugin_name = directory.rsplit('/')[-2]
+                getattr(importlib.import_module(plugin_name), '__init__')()
