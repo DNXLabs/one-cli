7b4bc2a36f60caa6e0816b51f7cf75fcddf7f84c
Arthur Diniz <arthurbdiniz@gmail.com>
2020-10-07 22:00:00 -0300

    Add to shell command --port option
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- one/commands/shell.py
+++ one/commands/shell.py
@@ -13,6 +13,7 @@ SHELL_IMAGE = image.get_image('shell')
 @click.command(help='Shell container with awscli and terraform pre-installed.')
 @click.argument('args', nargs=-1)
 @click.option('-i', '--image', default=SHELL_IMAGE, type=str, help='Docker image to use.')
+@click.option('-p', '--port', default=(), type=str, help='Ports to expose from the container.', multiple=True)
 def shell(args, image, port):
     envs = environment.build().get_env()
     command = ''
@@ -20,9 +21,12 @@ def shell(args, image, port):
     for arg in args:
         command += '%s ' % (arg)
 
+    ports = list(port)
+
     container.create(
         image=image,
         command=command,
+        ports=ports,
         entrypoint='',
         volumes=['.:/work'],
         environment=envs
