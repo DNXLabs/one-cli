8f8c4970469d00fdbb604ee73967f19854fd42a3
Felipe Rodrigues <57083757+sohflp@users.noreply.github.com>
2020-10-23 04:04:13 +1100

    Add Okta SSO to list of IDP providers (#98)
    - Associate new docker image dnxsolutions/aws-okta-auth
    - Create new command one auth okta for authentication with new IDP
    - Create new command one auth configure for okta IDP parameters configuration
    - Add new resources in .gitignore

--- one/prompt/idp.py
+++ one/prompt/idp.py
@@ -1,4 +1,10 @@
-prompt_providers = ['Google G Suite SSO', 'Microsoft Azure SSO', 'AWS SSO', 'AWS IAM user']
+prompt_providers = [
+    'Google G Suite SSO',
+    'Microsoft Azure SSO',
+    'Okta SSO',
+    'AWS SSO',
+    'AWS IAM user'
+]
 
 PROVIDER_QUESTIONS = [
     {
@@ -35,6 +41,27 @@ AZURE_QUESTIONS = [
         'type': 'input',
         'name': 'AZURE_APP_ID_URI',
         'message': 'What\'s your AZURE_APP_ID_URI credential:',
+        'validate': lambda text: len(text) >= 4 or 'Must be at least 4 characters.'
+    }
+]
+
+OKTA_QUESTIONS = [
+    {
+        'type': 'input',
+        'name': 'OKTA_ORG',
+        'message': 'What\'s your OKTA_ORG credential:',
+        'validate': lambda text: len(text) >= 4 or 'Must be at least 4 characters.'
+    },
+    {
+        'type': 'input',
+        'name': 'OKTA_AWS_APP_URL',
+        'message': 'What\'s your OKTA_AWS_APP_URL credential:',
+        'validate': lambda text: len(text) >= 4 or 'Must be at least 4 characters.'
+    },
+    {
+        'type': 'input',
+        'name': 'OKTA_AWS_DEFAULT_REGION',
+        'message': 'What\'s your OKTA_AWS_DEFAULT_REGION credential:',
         'validate': lambda text: len(text) >= 4 or 'Must be at least 4 characters.'
     }
 ]
