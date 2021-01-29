cdaf8b6462eac8f4ea9643f9ace4e8c2c945ad50
Arthur Diniz <arthurbdiniz@gmail.com>
2021-01-29 18:25:33 -0300

    New plugin architecture
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- README.md
+++ README.md
@@ -161,32 +161,14 @@ workspaces:
 
 To give better support for customization inside the CLI we created a `plugin system` that you can extend code, creating new commands and groups and even modify the existing ones.
 
-All plugins need to be created inside ` ~/.one/plugins/*`
+All plugins need to be python packages to work.
 
-#### Folder Structure
+#### List of plugins
 
-```bash
-└── plugins
-    ├── __init__.py (empty file)
-    └── my_plugin.py
-```
-
-#### Plugin Example
-
-`~/.one/plugins/my_plugin.py`
-```python
-import click
-from one.one import cli
-
-
-def __init__():
-    cli.add_command(my_plugin)
-
-
-@click.command(name='my_plugin', help='My plugin command')
-def my_plugin():
-    click.echo('It works!')
-```
+- [plugin-commands](https://github.com/DNXLabs/plugin-commands)
+- [plugin-kube](https://github.com/DNXLabs/plugin-kube)
+- [plugin-dnx-assume](https://github.com/DNXLabs/plugin-dnx-assume)
+- [plugin-foundation](https://github.com/DNXLabs/plugin-foundation)
 
 #### Running
 
--- one/utils/plugins.py
+++ one/utils/plugins.py
@@ -1,100 +1,64 @@
-import glob
 import importlib
 import sys
-import os
+import subprocess
 import click
-import json
-import tarfile
-import shutil
 import yaml
-from os.path import join
-from os import path
-import urllib.request
-from pathlib import Path
-from one.utils.config import get_config_value
+import importlib.util
 from one.__init__ import CONFIG_FILE
 
 
-home = str(Path.home())
-TEMP_FOLDER = '.one/.temp'
-PLUGIN_DATA_FILE = '.one/plugins.json'
+def cleanup(garbage_packages):
+    for garbage in garbage_packages:
+        try:
+            subprocess.check_call([sys.executable, "-m", "pip", "uninstall", garbage, "-y"])
+        except subprocess.CalledProcessError:
+            raise SystemExit
 
 
-def download_plugin(extract_source, url, key):
-    if not path.exists('.one'):
-        os.mkdir('.one')
-
-    file_stream = urllib.request.urlopen(url)
-
-    if url.endswith('tar.gz'):
-        tar = tarfile.open(fileobj=file_stream, mode='r|gz')
-        tar.extractall(TEMP_FOLDER)
-        tar.close()
-    elif url.endswith('tar'):
-        tar = tarfile.open(fileobj=file_stream, mode='r|')
-        tar.extractall(TEMP_FOLDER)
-        tar.close()
-    else:
-        click.echo(
-            click.style('ERROR ', fg='red') +
-            'No supporot for this file format.\n'
-        )
+def get_installed_packages():
+    try:
+        reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+        installed_packages = [r.decode() for r in reqs.split()]
+        return installed_packages
+    except subprocess.CalledProcessError:
         raise SystemExit
 
-    plugin_path = join(extract_source, key)
-    if path.exists(plugin_path):
-        shutil.rmtree(plugin_path)
 
-    shutil.move(glob.glob('.one/.temp/*')[0], plugin_path)
-    shutil.rmtree(TEMP_FOLDER)
-
-
-def cleanup_plugins(extract_source, installed_plugins):
-    if not path.exists(extract_source):
-        return
-
-    set_difference = set(os.listdir(extract_source)) - set(installed_plugins.keys())
-    list_difference = list(set_difference)
-    for directory in list_difference:
-        shutil.rmtree(join(extract_source, directory))
-
-
-def read_plugin_data():
+def install(package):
     try:
-        with open(PLUGIN_DATA_FILE) as json_file:
-            return json.load(json_file)
-    except Exception:
-        return {}
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    except subprocess.CalledProcessError:
+        raise SystemExit
 
 
-def check_plugins(extract_source='.one/plugins/'):
+def load_plugins():
+    installed_packages = get_installed_packages()
+    garbage_packages = [i for i in installed_packages if 'one-cli-plugin-' in i]
     try:
-        installed_plugins = {}
-        plugin_data = read_plugin_data()
-
         with open(CONFIG_FILE) as file:
             docs = yaml.load(file, Loader=yaml.BaseLoader)
-            for key, url in docs['plugins'].items():
-                source = get_config_value('plugins.' + key + '.source')
-                plugin_path = join(extract_source, key)
-                if not path.exists(plugin_path) or plugin_data[key]['source'] != source:
-                    click.echo('Installing plugin %s.' % key)
-                    download_plugin(extract_source, source, key)
-                    click.echo('Plugin %s successfully installed.\n' % key)
+            for plugin in docs['plugins']:
+                package = plugin.get('package')
+                module = plugin.get('module')
 
-                plugin = {
-                    'source': source,
-                    'dir': join(extract_source, key)
-                }
-                installed_plugins[key] = plugin
+                if package not in installed_packages:
+                    click.echo('Installing plugin %s.' % package)
+                    install(package)
+                    click.echo('Plugin %s successfully installed.\n' % package)
+                else:
+                    garbage_packages.remove(package)
+
+                getattr(importlib.import_module(module), '__init__')()
+
         file.close()
     except KeyError:
         pass
     except AttributeError:
         click.echo(
-            click.style('WARN ', fg='yellow') +
-            'Plugin block declared but empty.\n'
+            click.style('ERROR ', fg='red') +
+            'Plugin attribute error.\n'
         )
+        raise SystemExit
     except FileNotFoundError:
         click.echo(
             click.style('ERROR ', fg='red') +
@@ -105,30 +69,6 @@ def check_plugins(extract_source='.one/plugins/'):
             click.style('ERROR ', fg='red') +
             'Unexpected error.\n'
         )
-        raise
+        raise SystemExit
 
-    update_file_and_folder(installed_plugins, extract_source)
-    cleanup_plugins(extract_source, installed_plugins)
-
-
-def update_file_and_folder(installed_plugins={}, extract_source=None):
-    if installed_plugins == {}:
-        try:
-            os.remove(PLUGIN_DATA_FILE)
-            shutil.rmtree(extract_source)
-        except Exception:
-            pass
-    else:
-        with open(PLUGIN_DATA_FILE, 'w') as outfile:
-            json.dump(installed_plugins, outfile)
-
-
-def load_plugins(source='.one/plugins/'):
-    check_plugins()
-
-    if path.exists(source):
-        sys.path.append('.one')
-        for directory in glob.glob(join(source, '*/')):
-            if path.exists(directory + '__init__.py'):
-                command_path = '.'.join(directory.rsplit('/', 3)[1:])[:-1] + '.__init__'
-                getattr(importlib.import_module(command_path), '__init__')()
+    cleanup(garbage_packages)
